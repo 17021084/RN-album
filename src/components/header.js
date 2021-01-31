78bdb6594d9e4f07b746d7f3340f4c0d667450b3
17021084 <dotrunghg1999@gmail.com>
2021-01-31 17:02:13 +0700

    create header component

---
+++ src/components/header.js
@@ -0,0 +1,10 @@
+// import libraries
+import React from 'react';
+import {Text} from 'react-native';
+
+//make component
+const Header = () => {
+  return <Text> this is Header </Text>;
+};
+//export
+export default Header;
