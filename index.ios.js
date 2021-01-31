74614044e0ba388a3d7694ff7ef2fe9350e9eafa
17021084 <dotrunghg1999@gmail.com>
2021-01-31 22:58:32 +0700

    Album Class component

--- index.ios.js
+++ index.ios.js
@@ -1,11 +1,17 @@
 // this index.ios.js - place for IOS code
-
 // Import library
 import React from 'react';
-import {AppRegistry} from 'react-native';
+import {AppRegistry, View} from 'react-native';
 import Header from './src/components/header';
+import AlbumList from './src/components/albumList';
+
 // Created componence
-const App = () => <Header headerText={'Albums!!'} />;
+const App = () => (
+  <View>
+    <Header headerText={'Albums!!'} />
+    <AlbumList />
+  </View>
+);
 
 // Render it on device
 AppRegistry.registerComponent('albums', () => App);
