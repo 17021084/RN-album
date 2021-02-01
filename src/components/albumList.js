f23fd9629e572a5ddd50dc8a40286abc24e4aedd
17021084 <dotrunghg1999@gmail.com>
2021-02-01 13:59:25 +0700

    render list

--- src/components/albumList.js
+++ src/components/albumList.js
@@ -1,11 +1,38 @@
 import React, {Component} from 'react';
 import {View, Text} from 'react-native';
+import axios from 'axios';
 
 class AlbumList extends Component {
+  state = {albums: []};
+  componentDidMount() {
+    axios
+      .get('http://localhost:5555/music_albums')
+      .then((response) => {
+        this.setState({albums: response.data});
+      })
+      .catch((error) => console.log(error));
+  }
+
+  //deprecated , ko nen call api tu day
+  //   componentWillMount() {
+  //     console.log('will mount');
+  //   }
+  renderAlbum() {
+    return this.state.albums.map((album) => (
+      <View key={album.id}>
+        <Text>{album.title}</Text>
+        <Text>{album.artist}</Text>
+        <Text>{album.url}</Text>
+        <Text>{album.image}</Text>
+        <Text>{album.thumbnail_image}</Text>
+      </View>
+    ));
+  }
   render() {
     return (
       <View>
-        <Text> Album list </Text>
+        <Text> Album list abc !!!!</Text>
+        {this.renderAlbum()}
       </View>
     );
   }
